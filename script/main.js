f30f5773be2f8b910fc4d5bd812ba2b344d2c275
Apu Mandal <60982235+Hack6rapu@users.noreply.github.com>
2022-11-08 21:40:10 +0530

    Final

--- script/main.js
+++ script/main.js
@@ -304,21 +304,3 @@ const resolveFetch = () => {
 };
 
 resolveFetch().then(animationTimeline());
-
-
-//for audio
-var audio = document.getElementById('audio');
-var playPauseBTN = document.getElementById('playPauseBTN');
-var count = 0;
-
-function playPause(){
-	if(count == 0){
-		count = 1;
-		audio.play();
-		playPauseBTN.innerHTML = "Pause ⏸";
-	}else{
-		count = 0;
-		audio.pause();
-		playPauseBTN.innerHTML = "Play ►";
-	}
-}
